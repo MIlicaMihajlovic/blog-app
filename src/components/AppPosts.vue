924a1fdc6d0d513f6f86466ad2b1dd37f962c372
MIlicaMihajlovic <m.mihajlovicc@yahoo.com>
2018-11-30 19:30:48 +0100

    List of posts

--- src/components/AppPosts.vue
+++ src/components/AppPosts.vue
@@ -1,11 +1,32 @@
 <template>
     <div>
-
+        <ul class="list-unstyled">
+            <li v-for="(post, index) in posts" :key="index">
+                {{ post.title }}
+            </li>
+        </ul>
+        
     </div>
 </template>
 
 <script>
+
+import { posts } from '../service/posts.js'
+
 export default {
+    
+    data() {
+        return {
+            posts: []
+        }
+    },
+
+    created() {
+        posts.getAll()
+            .then(response => {
+                this.posts = response.data
+            })
+    }
     
 }
 </script>
