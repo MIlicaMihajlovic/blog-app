dd83cd4153aadc727241ca20614e8e1fe8e87b24
MIlicaMihajlovic <m.mihajlovicc@yahoo.com>
2018-11-30 19:01:07 +0100

    Add posts and layout

---
+++ src/router.js
@@ -0,0 +1,15 @@
+import VueRouter from 'vue-router';
+import Vue from 'vue';
+import AppPosts from './components/AppPosts.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+    {path: '/', redirect: '/posts'},
+    {path: '/posts', component: AppPosts, name: 'posts'}
+]
+
+export const router = new VueRouter({
+    mode: 'history',
+    routes  
+});
